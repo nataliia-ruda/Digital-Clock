167e6da094b40b74702d29ac3cc6d398d37cda98
Nataliia Ruda <nataliiar754@gmail.com>
2025-02-06 23:09:00 +0100

    created files

---
+++ src/Clock.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react"
+
+const Clock = () => {
+  return (
+    <section className="clock-container">
+        <div className="clock">
+          <span>00:00:00</span>
+        </div>
+    </section>
+  )
+}
+
+export default Clock
